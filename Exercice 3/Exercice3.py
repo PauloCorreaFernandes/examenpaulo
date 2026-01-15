d9e84c7aa17e97a17059e7271fc013fc56dd09c9
PauloCorreaFernandes <pauloebiafernandes@hotmail.com>
2026-01-15 15:45:09 -0500

    correction du code parce , parce que j'ai melange avec les exercices 2 et jai cree fichier resultas.txt par erreur , donc maintenant jai supprimer le fichier

--- Exercice 3/Exercice3.py
+++ Exercice 3/Exercice3.py
@@ -1,7 +1,6 @@
 #Examen Final : Exercice 3
 #Programme qui demande 10 entiers et calcule
 #le minimum , le maximum , la somme et la moyenne
-#Les resultats sont conserves dans le fichier  resultats.txt
 
 class Outils:
     def __init__(self):
