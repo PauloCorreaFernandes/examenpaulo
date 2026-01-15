a81f9b1c9645cc31ec20c974653913745665e216
PauloCorreaFernandes <pauloebiafernandes@hotmail.com>
2026-01-15 14:47:35 -0500

    #liste pour stocker les entiers

---
+++ Exercice 3/Exercice3.py
@@ -0,0 +1,10 @@
+#Examen Final : Exercice 3
+
+#Programme qui demande 10 entiers et calcule
+#le minimum , le maximum , la somme et la moyenne
+#Les resultats sont conserves dans le fichier  resultats.txt
+
+class Outils
+    def __init__(self):
+        #liste pour stocker les entiers
+        self.nombres = []
