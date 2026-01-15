0a34ba8cb2788081011710a2dda331c68f42fc85
PauloCorreaFernandes <pauloebiafernandes@hotmail.com>
2026-01-15 16:03:24 -0500

    PyQt6

---
+++ Exercice 2/Exercice 2.py
@@ -0,0 +1,7 @@
+# programme que utilise le bibliotheque vu encalsse ...
+import sys
+
+#pip install PyQt6
+from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QGridLayout, QLineEdit)
+
+FICHIER = "resultats.txt"
