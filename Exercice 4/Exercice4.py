ef134b2e5c08b6a32e84667856b05a6dc01070ac
PauloCorreaFernandes <pauloebiafernandes@hotmail.com>
2026-01-15 17:33:12 -0500

    #Methode de presentation

--- Exercice 4/Exercice4.py
+++ Exercice 4/Exercice4.py
@@ -2,3 +2,13 @@
 
 class Personne:
     def __init__(self, nom):
+        #Attribu nom
+        self.nom = nom
+
+    def se_presenter(self):
+        #Methode de presentation
+        return " Je m'appelle " + self.nom
+
+
+
+
