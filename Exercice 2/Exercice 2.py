64d9813399da44a1d2dd261c9448f54a2dc9fbd7
PauloCorreaFernandes <pauloebiafernandes@hotmail.com>
2026-01-15 17:00:47 -0500

    Correction final partie 1

--- Exercice 2/Exercice 2.py
+++ Exercice 2/Exercice 2.py
@@ -50,12 +50,14 @@ def charger_resultat():
             valeur = int(contenu)
             le_double.setText(str(valeur))
             lbl_msg.setText("Resultat charge depuis resultats.txt")
-
-        except FileNotFoundError:
+        except ValueError:
             lbl_msg.setText("Erreur: contenu du fichier invalide")
-        except Exception as e:
+
+    except FileNotFoundError:
+        lbl_msg.setText("Erreur: Resuktats.txt n'existe pas")
+    except Exception as e:
             print("Erreur chargement :", e)
-            lbl_msg.setText("Erreur: Impossible de charger !")
+        lbl_msg.setText("Erreur: Impossible de charger !")
 
 #1 - Creer un objet application
 app = QApplication([])
