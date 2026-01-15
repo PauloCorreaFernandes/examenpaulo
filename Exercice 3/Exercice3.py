583dd5576a02b1a41087ed0b7a53c69b70cda475
PauloCorreaFernandes <pauloebiafernandes@hotmail.com>
2026-01-15 14:50:35 -0500

    #initialiser le minimum avec le premier nombre

--- Exercice 3/Exercice3.py
+++ Exercice 3/Exercice3.py
@@ -4,7 +4,13 @@
 #le minimum , le maximum , la somme et la moyenne
 #Les resultats sont conserves dans le fichier  resultats.txt
 
-class Outils
+class Outils:
     def __init__(self):
         #liste pour stocker les entiers
         self.nombres = []
+
+    def saisir(selfself):
+        #demander 10 entiers au usager
+        for i in range(1,11):
+            n= int(input("Tapez un nombre entier(" + str(i) + "/10) : "))
+            self.nombres.append(n)
