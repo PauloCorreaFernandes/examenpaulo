63bf39586a62d9997e09a518d123135f4fc60f5b
PauloCorreaFernandes <pauloebiafernandes@hotmail.com>
2026-01-15 14:22:33 -0500

    # essaye de corrige le triangle

--- Exercice1/Exercice1.py
+++ Exercice1/Exercice1.py
@@ -10,7 +10,7 @@ class Triangle:
         #afficher deux triangles cote a cote
         for i in range(1, self.n + 1):
             gauche= "*" * i
-            espaces= " " * (self.n + 2)
+            espaces= " " * (2 *(self.n - i) +4)
             droite= "*" * i
             print(gauche + espaces + droite)
 
