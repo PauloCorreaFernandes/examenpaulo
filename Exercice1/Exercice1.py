c2d92303f20d72479c57e88f429e8a1135134849
PauloCorreaFernandes <pauloebiafernandes@hotmail.com>
2026-01-15 13:43:13 -0500

    methode qui affiche le triangle a droite

--- Exercice1/Exercice1.py
+++ Exercice1/Exercice1.py
@@ -7,8 +7,13 @@ class Triangle:
         self.n = n
 
     def triangle_gauche(self):
-    #methode qui affiche le triangle de gauche
+    #methode qui affiche le triangle a gauche
         symbol = '*'
         for i in range(self.n):
             print(symbol)
             symbol = symbol + '*'
+
+    def triangle_droite(self):
+    #methode  qui affiche le triangle a droite
+        for in range(1, self.n + 1):
+            print('*' * i)
